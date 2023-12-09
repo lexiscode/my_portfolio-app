c30c75fa547ebd9a127ad38c56bb04a9dbf0ed8b
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-12-09 10:17:57 +0100

    feat: worked on the resume page

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -11,7 +11,7 @@ class ContactController extends AbstractController
     #[Route('/contact', name: 'app_contact')]
     public function index(): Response
     {
-        return $this->render('contact/index.html.twig', [
+        return $this->render('frontend/contact/index.html.twig', [
             'controller_name' => 'ContactController',
         ]);
     }
