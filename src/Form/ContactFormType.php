a54251f595681fa8be2114fd8361945edc021070
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-12-10 14:57:01 +0100

    feat: added form validations in the contact fields

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -5,6 +5,8 @@ namespace App\Form;
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -17,18 +19,36 @@ class ContactFormType extends AbstractType
         $builder
             ->add('name', TextType::class, [
                 'attr' => ['class' => 'pointer-input', 'placeholder' => 'Your name', 'type' => 'text'],
-                'constraints' => [new NotBlank()],
+                'constraints' => [
+                    new NotBlank(),
+                    new Length([
+                        'min' => 3, 
+                        'max' => 100, 
+                        'minMessage' => 'Your name must be at least {{ limit }} characters long.',
+                        'maxMessage' => 'Your name cannot be longer than {{ limit }} characters.',
+                    ]),
+                ],
             ])
             ->add('email', TextType::class, [
                 'attr' => ['class' => 'pointer-input', 'placeholder' => 'Your email', 'type' => 'email'],
-                'constraints' => [new NotBlank()],
+                'constraints' => [
+                    new NotBlank(),
+                    new Email(['message' => 'The email "{{ value }}" is not a valid email.']),
+                ],
             ])
             ->add('subject', TextType::class, [
+                'required' => false,
                 'attr' => ['class' => 'pointer-input', 'placeholder' => 'Subject', 'type' => 'text'],
             ])
             ->add('message', TextareaType::class, [
                 'attr' => ['class' => 'pointer-input', 'placeholder' => 'Your message', 'type' => 'text', 'rows' => '3'],
-                'constraints' => [new NotBlank()],
+                'constraints' => [
+                    new NotBlank(),
+                    new Length([
+                        'min' => 3, 
+                        'minMessage' => 'Your name must be at least {{ limit }} characters long.',
+                    ]),
+                ],
             ])
         ;
     }
