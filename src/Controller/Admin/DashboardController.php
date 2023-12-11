584d08c6ae6f92b8f8e202f951a590012df06b12
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-12-11 08:27:45 +0100

    feat: organized the admin template in the backend folder and created other necessary subfolders

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -1,18 +1,20 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_USER')]
 class DashboardController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
+    #[Route('/admin/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
         return $this->render('backend/dashboard/index.html.twig', [
-            'controller_name' => 'DashboardController',
+            'controller_name' => 'AboutController',
         ]);
     }
 }
