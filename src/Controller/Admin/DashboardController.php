272f2351e65938f9e0d7d5f3b12e573bab0f585b
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-12-12 18:41:48 +0100

    fix: solved the mails variable error for the admin profile panel in specific pages

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,19 +2,33 @@
 
 namespace App\Controller\Admin;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Contact;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[IsGranted('ROLE_USER')]
 class DashboardController extends AbstractController
 {
+    private $em;
+    private $mailRepository;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+        $this->mailRepository = $em->getRepository(Contact::class);
+    }
+    
     #[Route('/admin/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
+        // Retrieve mails from the database
+        $mails = $this->mailRepository->findAll();
+
         return $this->render('backend/dashboard/index.html.twig', [
-            'controller_name' => 'AboutController',
+            'mails' => $mails,
         ]);
     }
 }
