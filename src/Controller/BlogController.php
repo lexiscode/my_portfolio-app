fb7335213f52c5075fe9ae45047d259e0d2af468
Nwokorie Alex V <nwokoriealex20@gmail.com>
2023-12-16 14:50:46 +0100

    feat: added pagination in blogs page, and made other code adjustments

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -7,26 +7,36 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class BlogController extends AbstractController
 {
 
     private $blogRepository;
+    private $paginator;
     
-    public function __construct(EntityManagerInterface $em)
+    public function __construct(EntityManagerInterface $em, PaginatorInterface $paginator)
     {
         $this->blogRepository = $em->getRepository(Blog::class);
+        $this->paginator = $paginator;
     }
 
     #[Route('/blog', name: 'app_blog')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         // Retrieve blogs from the database
         $blogs = $this->blogRepository->findAll();
 
+        $pagination = $this->paginator->paginate(
+            $blogs,
+            $request->query->getInt('page', 1), // Get the current page or default to 1
+            9 // Items per page
+        );
+
         return $this->render('frontend/blog/index.html.twig', [
             'blogs' => $blogs,
+            'pagination' => $pagination,
         ]);
     }
 
